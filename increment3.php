657783d71759fcf6cad044626fc48f0099c1a435
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-09-01 18:54:35 +0530

    php increment

---
+++ increment3.php
@@ -0,0 +1,27 @@
+<?php 
+
+    // here we see the increment and decrement in the php
+
+    $counter = null;
+    
+    $counter++;
+    echo" {$counter} <br>";
+    
+    $counter++;
+    
+    echo" {$counter} <br>";
+
+
+    // decrement 
+
+    $counter--;
+    echo" {$counter} <br>";
+    
+    $counter+=10;
+    echo" {$counter} <br>";
+
+    $counter-=100;
+    echo" {$counter} <br>";
+
+
+?>
