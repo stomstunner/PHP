d6d13c9c30488a2df5afca4702f01ad9826ad233
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-09-01 18:54:22 +0530

    php airthmetic

---
+++ airthmatic2.php
@@ -0,0 +1,37 @@
+<?php 
+    echo"Hello world<br><br>";
+
+    // airthmetic operations 
+    $x = 10;
+    $y = 3;
+    $z = null ;
+
+    // addition
+    // $z = $x + $y;
+    // echo $z;
+
+    // substraction
+    // $z = $x - $y;
+    // echo $z;
+
+
+    // multiplication
+    // $z = $x * $y;
+    // echo $z;
+
+    // division
+    // $z = $x / $y;
+    // echo $z;
+
+
+    // power 
+    // $z = $x ** $y;
+    // echo $z;
+
+
+    // modulo 
+    // $z = $x % $y;
+    // echo $z;
+
+
+?>
