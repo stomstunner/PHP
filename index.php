18db06e75d9fa0ad798823ea9a7010c1f1548b36
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-09-01 17:51:57 +0530

    first php program

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// all the php code will be written in this angukar backet and question marks 
+
+// for the diplay we used echo 
+echo"I like pizza, sandwich and burger";
+// for line bracke we can use <br> 
+echo"<br> My name is ujjwal";
+
+    /* this
+        is 
+        an
+        multiline
+        comment
+    */
+?>
+
+<!-- also we can write the html code inside the php file  -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <br>
+    <br>
+    <button>Order Pizza</button>
+</body>
+</html>
