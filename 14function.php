53a711b37a54bb21f7962c33e004455b8cb45eaf
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-09-02 20:17:01 +0530

    function in php

---
+++ 14function.php
@@ -0,0 +1,16 @@
+<!-- here we learn about the fucntion in the php  -->
+<?php
+
+function helloWorld(){
+    echo"Happy birthday to you <br>";
+    echo"Happy birthday to you dear<br>";
+    echo"May you live for the tens of year<br>";
+    echo"Happy birthday to you hi     <br>";
+}
+
+
+// if we want to execute the fuc tiion in the php we just have to write the name of the fucnton with the paranethisis
+
+helloWorld();
+
+?>
