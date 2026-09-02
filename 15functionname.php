101bc24cd10900a5bfd39219c410fac76ab30e27
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-09-02 20:17:20 +0530

    fucntion in php

---
+++ 15functionname.php
@@ -0,0 +1,18 @@
+
+
+
+<?php
+/**
+ * @param string $name
+ * 
+ */
+
+function happyBirthday( string $name, int $age){
+    
+    echo"Happy birthday to you dear {$name} <br>";
+    echo"You are {$age} years old.<br>";
+}
+
+happyBirthday("Ujjwal", 21);
+
+?>
