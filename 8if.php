a155de52e9a87b7f1e06736b9e2bd1a9afb2cc3e
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-09-02 18:20:47 +0530

    if in php

---
+++ 8if.php
@@ -0,0 +1,30 @@
+<?php
+    $age = 21;
+
+    // if($age >= 18){
+    //     echo"You can vote";
+    // }
+    // else{
+    //     echo"Age must be 18+ for voting";
+    // }
+
+
+    // if($age == 18){
+    //     echo"You can vote";
+    // }
+    // elseif ($age == 21) {
+    //     echo"Your age is 21";
+    // }
+    // else{
+    //     echo"Age must be 18+ for voting";
+    // }
+
+    $adult = false;
+
+    if($adult == true){ // or ($adult)
+        echo"You can entered this site";
+    }
+    else{
+        echo"You cannot entered this site";
+    }
+?>
